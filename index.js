15a745d037d7d579bc3ff15530ff7d74cc48c921
MohamedElmdary <engm5081@gmail.com>
2022-03-13 17:06:42 +0200

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+function getUrl() {
+    const params = new URLSearchParams(window.location.search)
+    const id = params.get('id');
+    const path = window.location.hash.replace('#', '');
+    const origin = window.location.origin;
+    const name = window.location.pathname;
+    return origin + name + '/goodbye' + path + '?id=' + id;
+}
+
 function tfTracker(hooks) {
     hooks.ready(function () {
         window.addEventListener('beforeunload', function () {
-            fetch(location.href + '/goodbye')
+            fetch(getUrl())
                 .then(res => res.json())
                 .then(console.log)
                 .catch(console.error)
